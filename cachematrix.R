0aea141f5002db3120c6834d2de8af5acdc47277
spark <spark@Tien-Dung-LEs-MacBook-Pro.local>
2015-03-16 23:00:57 +0100

    First running version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix function creates a special "matrix" object 
+## that can cache its inverse.
 
-## Write a short comment describing this function
+## 1. set the value of the vector
+## 2. get the value of the vector
+## 3. set the value of the mean
+## 4. get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(invX) inv <<- invX
+  getInv <- function() inv
+  list(set=set, get=get, setInv = setInv, getInv = getInv)  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'`
+  inv <- x$getInv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  ##assume that the matrix supplied is always invertible.
+  inv <- solve(data)
+  x$setInv(inv)
+  inv
 }
+
+## Test case
+## m <- matrix(c(3,1,2,1), nrow=2, ncol=2)
+## mat = makeCacheMatrix(m)
+## mat$get()
+## cacheSolve(mat) ## first time
+## cacheSolve(mat) ## second time
+## m %*% cacheSolve(mat) 
+## m %*% cacheSolve(mat)
